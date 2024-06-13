dd2bef625036c42ad706397b9463300c4da8421d
Mohsen Karami <kara0158@algonquinlive.com>
2024-06-13 15:36:35 -0400

    write text

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const textarea = document.querySelector('textarea')
 copybtn.addEventListener('click', () =>{
     textarea.select()
     let textvalue = textarea.value
+
+    navigator.clipboard.writeText('textvalue')
 })
