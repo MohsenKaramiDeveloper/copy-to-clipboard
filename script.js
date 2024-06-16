8efa12e6d7364116c0cc22cb1d5e786834cf5ffd
Mohsen Karami <kara0158@algonquinlive.com>
2024-06-15 23:14:54 -0400

    color correction

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ copybtn.addEventListener('click', () =>{
 
     navigator.clipboard.writeText(textvalue)
     copybtn.innerText = 'copied !'
-    copybtn.style.backgroundColor = '#B954DB'
+    copybtn.style.backgroundColor = '#011140'
 
     setTimeout(() =>{
         copybtn.innerText = 'Copy'
