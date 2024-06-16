48b2f4dc4e4d1b4c142110de6677cd793f8c024f
Mohsen Karami <kara0158@algonquinlive.com>
2024-06-15 23:16:16 -0400

    line 24

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const copybtn = document.querySelector('button')
-const textarea = document.querySelector('textarea')
+// const copybtn = document.querySelector('button')
+// const textarea = document.querySelector('textarea')
 
-copybtn.addEventListener('click', () =>{
-    textarea.select()
-    let textvalue = textarea.value
+// copybtn.addEventListener('click', () =>{
+//     textarea.select()
+//     let textvalue = textarea.value
 
-    navigator.clipboard.writeText(textvalue)
-    copybtn.innerText = 'copied !'
-    copybtn.style.backgroundColor = '#011140'
+//     navigator.clipboard.writeText(textvalue)
+//     copybtn.innerText = 'copied !'
+//     copybtn.style.backgroundColor = '#011140'
 
-    setTimeout(() =>{
-        copybtn.innerText = 'Copy'
-        copybtn.style.backgroundColor = '#1E0BDB'
+//     setTimeout(() =>{
+//         copybtn.innerText = 'Copy'
+//         copybtn.style.backgroundColor = '#1E0BDB'
         
-    }, 2000)
+//     }, 2000)
+
+
+
+    
+// })
+
+
+const copybtn = document.querySelector('button')
+// const textarea = document.querySelector('textarea')
+
+// copybtn.addEventListener('click', () =>{
+//     textarea.select()
+//     let textvalue = textarea.value
+
+//     navigator.clipboard.writeText(textvalue)
+//     copybtn.innerText = 'copied !'
+//     copybtn.style.backgroundColor = '#011140'
+
+//     setTimeout(() =>{
+//         copybtn.innerText = 'Copy'
+//         copybtn.style.backgroundColor = '#1E0BDB'
+        
+//     }, 2000)
 
 
 
