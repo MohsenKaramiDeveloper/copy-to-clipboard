165b2539e4d5954bb66e7ba02bcd8a27f89b4f90
Mohsen Karami <kara0158@algonquinlive.com>
2024-06-17 22:17:33 -0400

    script 3 deleted

--- script.js
+++ script.js
@@ -27,23 +27,7 @@
 
 
 
-const copyBtn = document.querySelector('.copy-btn');
-const textarea = document.querySelector('textarea');
 
-copyBtn.addEventListener('click', () => {
-    textarea.select();
-    document.execCommand('copy'); // Copy text to clipboard
-
-    // Change button text and style
-    copyBtn.innerText = 'Copied!';
-    copyBtn.classList.add('clicked');
-
-    // Reset button text and style after 2 seconds
-    setTimeout(() => {
-        copyBtn.innerText = 'Copy';
-        copyBtn.classList.remove('clicked');
-    }, 2000);
-});
 
 
 
