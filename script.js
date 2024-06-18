d1c7de443468647a258cc5bb760983753d15b201
Mohsen Karami <kara0158@algonquinlive.com>
2024-06-17 22:13:48 -0400

    add new script

--- script.js
+++ script.js
@@ -26,3 +26,32 @@
 
 
 
+const copybtn = document.querySelector('button');
+const textarea = document.querySelector('textarea');
+
+copybtn.addEventListener('click', () => {
+    textarea.select();
+    let textvalue = textarea.value;
+
+    navigator.clipboard.writeText(textvalue)
+        .then(() => {
+            copybtn.innerText = 'copied !';
+            copybtn.classList.add('button-animation'); // Add animation class
+            copybtn.style.backgroundColor = '#011140';
+            copybtn.style.color = '#434B58';
+
+            setTimeout(() => {
+                copybtn.innerText = 'Copy';
+                copybtn.style.backgroundColor = '#1E0BDB';
+                copybtn.style.color = '#AB71AB';
+                copybtn.classList.remove('button-animation'); // Remove animation class
+            }, 2000);
+        })
+        .catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+});
+
+
+
+
